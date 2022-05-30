ed256b03b44f91928285d52cb34c6c99bf7946fd
Omikix <omikix@gmail.com>
2022-05-30 23:50:40 +0200

    add str modul

--- class_model.py
+++ class_model.py
@@ -10,26 +10,32 @@ class Crypto:
         :param coin: name of cryptocurrency (for example "bitcoin")
         :param currency: name of your currency (for example "usd")
         """
+        self.coin = coin
+        self.currency = currency
         self.url = f'https://api.coingecko.com/api/v3/coins/{coin}'
         self.response = requests.get(self.url)
         self.data = json.loads(self.response.content.decode('utf-8'))
         self.saved_crypto_price = self.data.get('market_data').get('current_price').get(currency)
         self.crypto_price = self.data.get('market_data').get('current_price').get(currency)
 
+    def __str__(self):
+        return f"{self.coin}: {self.crypto_price} {self.currency.upper()}"
+
     def checking_loop(self):
-        print(self.saved_crypto_price)
+        print(self)
         seconds = 0
         print("=" * 20)
         while True:
             mins, secs = divmod(seconds, 60)
             hours, mins = divmod(mins, 60)
-            timer = '{:02d}[h]:{:02d}[m]:{:02d}[s]'.format(hours, mins, secs)
+            day, hours = divmod(hours, 24)
+            timer = '{:02d}[day]{:02d}[h]:{:02d}[m]:{:02d}[s]'.format(day, hours, mins, secs)
             time.sleep(1)
             seconds += 1
             if str(timer)[13] == '0':
                 self.response = requests.get(self.url)
                 self.data = json.loads(self.response.content.decode('utf-8'))
-                self.crypto_price = self.data.get('market_data').get('current_price').get('pln')
+                self.crypto_price = self.data.get('market_data').get('current_price').get(self.currency)
                 if self.crypto_price > (self.saved_crypto_price * 1.02):
                     print(
                         f'In: {timer} your crypto gain {((self.crypto_price * 100 / self.saved_crypto_price) - 100):.2f}%,'
